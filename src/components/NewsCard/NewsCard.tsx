9019c9c11f69ba5584b55eefa3d550f734dab2ea
Victor <victorescalonag@gmail.com>
2022-09-09 14:18:00 -0500

    feat: add props to newCard component

--- src/components/NewsCard/NewsCard.tsx
+++ src/components/NewsCard/NewsCard.tsx
@@ -3,28 +3,38 @@ import "./newsCard.css";
 
 interface Props {
   isFavorite: boolean;
+  author: string;
+  date: string;
+  title: string;
+  newsUrl: string;
+  onClickFavorite: () => void;
 }
 
-const NewsCard: React.FC<Props> = ({ isFavorite }) => {
+const NewsCard: React.FC<Props> = ({
+  isFavorite,
+  author,
+  date,
+  title,
+  newsUrl,
+  onClickFavorite,
+}) => {
   const { HeartIcon, ClockIcon } = useComponents();
 
   return (
     <div className="news-card">
       <div className="news-card__content">
-        <a href="https://www.google.com" target="_blank" rel="noreferrer">
+        <a href={newsUrl} target="_blank" rel="noreferrer">
           <div className="news-card__date">
             <div className="news-card__date-icon">
               <ClockIcon />
             </div>
-            3 hours ago by Author
+            {date} by {author}
           </div>
-          <h2 className="news-card__title">
-            Yes, React is taking over front-end development. The question is why
-          </h2>
+          <h2 className="news-card__title">{title}</h2>
         </a>
       </div>
       <div className="news-card__fav-button">
-        <button className="fav-button">
+        <button className="fav-button" onClick={onClickFavorite}>
           <HeartIcon isFavorite={isFavorite} />
         </button>
       </div>
